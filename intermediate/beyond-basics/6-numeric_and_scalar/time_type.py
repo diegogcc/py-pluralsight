134f4cec3340423af396a32b4f639ef9cf50f9c2
Diego Campo <diegoc906@gmail.com>
2020-01-22 16:16:26 -0500

    Time type file added

--- intermediate/beyond-basics/6-numeric_and_scalar/time_type.py
+++ intermediate/beyond-basics/6-numeric_and_scalar/time_type.py
@@ -6,3 +6,12 @@ datetime.time(3, 1, 2)      # 3h 1m 2s
 datetime.time(3, 1, 2, 262) # 3h 1m 2s 262us
 
 datetime.time(hour=23, minute=59, second=59, microsecond=999999)
+
+t = datetime.time(10, 59, 15, 8923)
+t.hour              # 10
+t.minute            # 59
+t.second            # 15
+t.microsecond       # 8923
+
+t.isoformat()               # '10:59:15.8923'
+t.strftime('%Hh%Mm%Ss')     # '10h59m15s'
